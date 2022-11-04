3721917a89a152bc3a62f0b1d33f5d67b2c43fa8
Tomp03 <48441424+Tomp03@users.noreply.github.com>
2022-11-04 22:46:40 +0100

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,37 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export function superbase(url, key) {
+
+  const supabase = createClient(url, key);
+  console.log(supabase);
+
+  async function signup(mail, pass) {
+    let data  = await supabase.auth.signUp({
+      email: mail,
+      password: pass,
+    });
+
+    return data ;
+  }
+
+  async function login(email, password) {
+    let data = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    return data;
+  }
+
+  async function logout() {
+    let data = await supabase.auth.signOut();
+
+    return data;
+  }
+
+  return {
+    signup,
+    login,
+    logout,
+  };
+}
